e02e1679bb423c322e10689f67b8c4220efb3d1d
donaldjung <donaldjung23@gmail.com>
2024-01-06 13:43:40 -0500

    importing image assets url and locally

--- meta_frontend_cert/intro_to_react/image_assets/image_assets/src/App.js
+++ meta_frontend_cert/intro_to_react/image_assets/image_assets/src/App.js
@@ -2,12 +2,18 @@
 import './App.css';
 import logo from "./assets/logo.png"
 
+const imgurl = "https://www.48hourslogo.com/oss/attachments/2022/01/13/347418572/fce5b08add13e667d5855320c8ab6b39.png"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Importing image asset from asset folder.</h1>
+        <h1>Importing image asset from asset folder and image URL.</h1>
         <img src={logo} className="App-logo" alt="logo" />
+        <img 
+          src={require("./assets/logo.png")} alt="logo" />
+        <img
+          src={imgurl} alt="logo" />
+
        
       </header>
     </div>
