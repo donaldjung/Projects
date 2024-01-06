bb195d78b159f9d935da4d2e1a6142fc168ffe83
donaldjung <donaldjung23@gmail.com>
2024-01-06 17:44:57 -0500

    finished meta front end react basics

--- meta_frontend_cert/intro_to_react/mini_project/mini_project/src/App.js
+++ meta_frontend_cert/intro_to_react/mini_project/mini_project/src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import ReactPlayer from "react-player";
 import MyVideo from './MyVideo';
+import music2 from './assets/project.mp3'
+
+const music1 = new Audio(music2);
+
+function toggleMusic() {
+  if (music1.paused) {
+    music1.play();
+  } else {
+      music1.pause();
+  }
+  }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <button onClick={toggleMusic}>Click to Play Music</button>
         
         <MyVideo />
 
--- meta_frontend_cert/intro_to_react/mini_project/mini_project/src/MyVideo.js
+++ meta_frontend_cert/intro_to_react/mini_project/mini_project/src/MyVideo.js
@@ -2,7 +2,12 @@
 import ReactPlayer from "react-player";
 const MyVideo = () => {
     return (
-      <ReactPlayer url='https://www.youtube.com/watch?v=2KjwV3T_u04' />
+      <ReactPlayer 
+      url='https://www.youtube.com/watch?v=2KjwV3T_u04'
+      playing={false}
+      volume={0.25}
+      
+      />
     );
     }
 
